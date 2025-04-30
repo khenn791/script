6d3c3f2d4b7eb23c563ac53a8d8b407e2b255b34
khen <chakhenne@gmail.com>
2025-04-30 09:03:08 +0700

    Update 8blaggggg.lua

--- 8blaggggg.lua
+++ 8blaggggg.lua
@@ -1,34 +1,34 @@
-if not getgenv().settings then
-   return 
-end
+if executed then return end
 
-getgenv().services = {
+local services = {
     get_service = function(self, service_name)
         return cloneref(game:GetService(service_name))
     end
 }
 -- // services \\ --
-local players = getgenv().services:get_service("Players")
-local run_service = getgenv().services:get_service("RunService")
-local workspace = getgenv().services:get_service("Workspace")
-local user_input_service = getgenv().services:get_service("UserInputService")
+local players = services:get_service("Players")
+local run_service = services:get_service("RunService")
+local workspace = services:get_service("Workspace")
+local user_input_service = services:get_service("UserInputService")
 local tween_service = game:GetService("TweenService")
 local stats = game:GetService("Stats")
 local camera = workspace.CurrentCamera
 -- // vars \\ --
-getgenv().target = nil
-getgenv().silent_aiming_position = nil
+local target;
+local silent_aiming_position;
 local local_player = players.LocalPlayer
 local local_character = local_player.Character
 local local_root = local_character and local_character:FindFirstChild("HumanoidRootPart")
 
+
+
 local fov = Drawing.new("Circle")
-fov.Visible = getgenv().settings.visuals.fov.visible
-fov.Transparency = getgenv().settings.visuals.fov.transparency
-fov.Color = getgenv().settings.visuals.fov.color
-fov.Filled = getgenv().settings.visuals.fov.filled
-fov.Radius = getgenv().settings.visuals.fov.size
-fov.NumSides = getgenv().settings.visuals.fov.size * 100
+fov.Visible = settings.visuals.fov.visible
+fov.Transparency = settings.visuals.fov.transparency
+fov.Color = settings.visuals.fov.color
+fov.Filled = settings.visuals.fov.filled
+fov.Radius = settings.visuals.fov.size
+fov.NumSides = settings.visuals.fov.size * 100
 fov.Position = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
 
 local screen_gui = Instance.new("ScreenGui")
@@ -73,28 +73,28 @@ ui_stroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 
 local highlight = Instance.new("Highlight")
 
-getgenv().funcs = {}
+local funcs = {}
 
-function getgenv().funcs.knocked_check(character)
+function funcs.knocked_check(character)
     if not character then return false end
     local humanoid = character:FindFirstChildOfClass("Humanoid")
     if not humanoid then return false end
     return humanoid.Health < 10
 end
 
-function getgenv().funcs.force_field_check(character)
+function funcs.force_field_check(character)
     return character and character:FindFirstChildOfClass("ForceField") ~= nil
 end
 
-function getgenv().funcs.get_closest_player(radius)
+function funcs.get_closest_player(radius)
     if not local_root then return end
     local closest, distance = nil, math.huge
     local screen_center = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
     for index, player in pairs(players:GetPlayers()) do
         if player ~= local_player and player.Character then
             local character = player.Character
-            if getgenv().settings.checks.knocked and getgenv().funcs.knocked_check(character) then continue end
-            if getgenv().settings.checks.forcefield and getgenv().funcs.force_field_check(character) then continue end
+            if settings.checks.knocked and funcs.knocked_check(character) then continue end
+            if settings.checks.forcefield and funcs.force_field_check(character) then continue end
             local root = character:FindFirstChild("HumanoidRootPart")
             if root then
                 local vector, on_screen = camera:WorldToViewportPoint(root.Position)
@@ -110,7 +110,7 @@ function getgenv().funcs.get_closest_player(radius)
     return closest
 end
 
-function getgenv().funcs.recalculate_velocity(player, part_name)
+function funcs.recalculate_velocity(player, part_name)
     local character = player and player.Character
     if not character then return Vector3.zero end
     local part = character:FindFirstChild(part_name)
@@ -134,97 +134,97 @@ local_player.CharacterAdded:Connect(function(character)
     local_root = character:WaitForChild("HumanoidRootPart")
 end)
 
-function getgenv().funcs.is_target_in_air(player)
+function funcs.is_target_in_air(player)
     local character = player and player.Character
     if not character then return false end
     local humanoid = character:FindFirstChildOfClass("Humanoid")
     local root_part = character:FindFirstChild("HumanoidRootPart")
     if not humanoid or not root_part then return false end
-    local velocity = getgenv().funcs.recalculate_velocity(player, "HumanoidRootPart")
+    local velocity = funcs.recalculate_velocity(player, "HumanoidRootPart")
     return velocity.Y >= 2 and humanoid.FloorMaterial == Enum.Material.Air
 end
 
-function getgenv().funcs.get_aiming_position(player, hit_part, pred)
+function funcs.get_aiming_position(player, hit_part, pred)
     if not player or not player.Character then return Vector3.zero end
     local part = player.Character:FindFirstChild(hit_part)
     if not part then return Vector3.zero end
 
-    local velocity = getgenv().funcs.recalculate_velocity(player, hit_part)
+    local velocity = funcs.recalculate_velocity(player, hit_part)
     local prediction = Vector3.new(pred, pred, pred)
     local aim_position = part.Position + velocity * prediction
 
-    if getgenv().funcs.is_target_in_air(player) then
-        aim_position = aim_position + Vector3.new(0, getgenv().settings.combat.offsets.jump, 0)
+    if funcs.is_target_in_air(player) then
+        aim_position = aim_position + Vector3.new(0, settings.combat.offsets.jump, 0)
     end
 
     return aim_position or Vector3.zero
 end
 
 text_button.MouseButton1Click:Connect(function()
-    if getgenv().settings.combat.silent.mode == "Target" then
-        if getgenv().target then
-            getgenv().target = nil
+    if settings.combat.silent.mode == "Target" then
+        if target then
+            target = nil
             text_button.Text = "Locking: None"
         else
-            getgenv().target = getgenv().funcs.get_closest_player(getgenv().settings.visuals.fov.size)
-            text_button.Text = getgenv().target and "Locking: " .. getgenv().target.DisplayName or "Locking: None"
+            target = funcs.get_closest_player(settings.visuals.fov.size)
+            text_button.Text = target and "Locking: " .. target.DisplayName or "Locking: None"
         end
     else
-        getgenv().target = nil
+        target = nil
         text_button.Text = "Locking: Auto"
     end
 end)
 
 run_service.RenderStepped:Connect(function()
-    if getgenv().settings.combat.silent.auto_prediction or getgenv().settings.combat.camlock.auto_prediction then
+    if settings.combat.silent.auto_prediction or settings.combat.camlock.auto_prediction then
         local ping_stats = stats.Network.ServerStatsItem["Data Ping"]:GetValue()
         local auto_pred_value
         if ping_stats then
             auto_pred_value = 0.1 + ping_stats / 2000 + (ping_stats / 1000) * (ping_stats / 1500) * 1.025
         end
-        if getgenv().settings.combat.silent.auto_prediction then
-            getgenv().settings.combat.silent.prediction = auto_pred_value
+        if settings.combat.silent.auto_prediction then
+            settings.combat.silent.prediction = auto_pred_value
         end
-        if getgenv().settings.combat.camlock.auto_prediction then
-            getgenv().settings.combat.camlock.prediction = auto_pred_value
+        if settings.combat.camlock.auto_prediction then
+            settings.combat.camlock.prediction = auto_pred_value
         end
     end
 
-    if getgenv().settings.combat.silent.enabled then
-        if getgenv().settings.combat.silent.mode == "Silent" then
-            getgenv().target = getgenv().funcs.get_closest_player(getgenv().settings.visuals.fov.size)
+    if settings.combat.silent.enabled then
+        if settings.combat.silent.mode == "Silent" then
+            target = funcs.get_closest_player(settings.visuals.fov.size)
         end
         
-        if getgenv().settings.visuals.highlight.enabled and getgenv().target and getgenv().target.Character then
-            highlight.Parent = getgenv().target.Character
-            highlight.FillColor = getgenv().settings.visuals.highlight.fill_color
-            highlight.OutlineColor = getgenv().settings.visuals.highlight.outline_color
+        if settings.visuals.highlight.enabled and target and target.Character then
+            highlight.Parent = target.Character
+            highlight.FillColor = settings.visuals.highlight.fill_color
+            highlight.OutlineColor = settings.visuals.highlight.outline_color
         else 
             highlight.Parent = nil
         end
 
-        if getgenv().target then
-            getgenv().silent_aiming_position = getgenv().funcs.get_aiming_position(getgenv().target, getgenv().settings.combat.silent.hit_part, getgenv().settings.combat.silent.prediction)
+        if target then
+            silent_aiming_position = funcs.get_aiming_position(target, settings.combat.silent.hit_part, settings.combat.silent.prediction)
         else
-            getgenv().silent_aiming_position = Vector3.zero
+            silent_aiming_position = Vector3.zero
         end
     else
-        getgenv().silent_aiming_position = Vector3.zero
+        silent_aiming_position = Vector3.zero
     end
 
-    if getgenv().settings.combat.camlock.enabled and getgenv().target and getgenv().target.Character then
-        local aiming_position = getgenv().funcs.get_aiming_position(getgenv().target, getgenv().settings.combat.camlock.hit_part, getgenv().settings.combat.camlock.prediction)
+    if settings.combat.camlock.enabled and target and target.Character then
+        local aiming_position = funcs.get_aiming_position(target, settings.combat.camlock.hit_part, settings.combat.camlock.prediction)
         local target_cframe = CFrame.new(camera.CFrame.Position, aiming_position)
-        camera.CFrame = camera.CFrame:Lerp(target_cframe, getgenv().settings.combat.camlock.smoothness)
+        camera.CFrame = camera.CFrame:Lerp(target_cframe, settings.combat.camlock.smoothness)
     end
 
-    if getgenv().settings.combat.auto_air.enabled and getgenv().target and getgenv().target.Character then
-        if getgenv().funcs.is_target_in_air(getgenv().target) then
+    if settings.combat.auto_air.enabled and target and target.Character then
+        if funcs.is_target_in_air(target) then
             local tool = local_character and local_character:FindFirstChildOfClass("Tool")
             if tool then
                 tool:Activate()
             end
-            task.wait(getgenv().settings.combat.auto_air.delay)
+            task.wait(settings.combat.auto_air.delay)
         end
     end
 end)
@@ -237,15 +237,15 @@ old_namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...
     if not checkcaller() and call_method == "FireServer" then
         for index, arg in pairs(arguments) do
             if typeof(arg) == "Vector3" then
-                if getgenv().settings.combat.silent.enabled and getgenv().target and getgenv().silent_aiming_position then
-                    arguments[index] = getgenv().silent_aiming_position
+                if settings.combat.silent.enabled and target and silent_aiming_position then
+                    arguments[index] = silent_aiming_position
                 end
                 return old_namecall(self, unpack(arguments))
             elseif typeof(arg) == "table" then
                 for sub_index, value in pairs(arg) do
                     if typeof(value) == "Vector3" then
-                        if getgenv().settings.combat.silent.enabled and getgenv().target and getgenv().silent_aiming_position then
-                            arg[sub_index] = getgenv().silent_aiming_position
+                        if settings.combat.silent.enabled and target and silent_aiming_position then
+                            arg[sub_index] = silent_aiming_position
                         end
                         return old_namecall(self, unpack(arguments))
                     end
@@ -257,18 +257,4 @@ old_namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...
     return old_namecall(self, ...)
 end))
 
-local old_new_index
-old_new_index = hookmetamethod(game, "__newindex", newcclosure(function(self, index, value)
-    if not checkcaller() and self:IsA("Humanoid") then
-        if index == "WalkSpeed" and getgenv().settings.misc.player.walk_speed.enabled then
-            return old_new_index(self, index, getgenv().settings.misc.player.walk_speed.value)
-        elseif index == "JumpPower" and getgenv().settings.misc.player.jump.enabled then
-            return old_new_index(self, index, getgenv().settings.misc.player.jump.value)
-        end
-    end
-    return old_new_index(self, index, value)
-end))
-
-task.wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/aa8283/i/refs/heads/main/Protected_2547730226887233.txt"))() 
-
+getgenv().executed = true
